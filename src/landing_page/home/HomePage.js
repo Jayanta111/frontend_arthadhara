22b29d365057ed71b1f4b3308124bdb227a0262a
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-10-28 01:45:55 +0530

    Updated

--- src/landing_page/home/HomePage.js
+++ src/landing_page/home/HomePage.js
@@ -7,37 +7,7 @@ import Eduction from './Eduction'
 import OpenAccount from '../OpenAccount'
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
 function HomePage() {
-  const navigate = useNavigate();
-  const [cookies, removeCookie] = useCookies([]);
-  const [username, setUsername] = useState("");
-  useEffect(() => {
-    const verifyCookie = async () => {
-      if (!cookies.token) {
-        navigate("/login");
-      }
-      const { data } = await axios.post(
-        "https://backend-pi-gilt-mhy8gp759b.vercel.app/",
-        {},
-        { withCredentials: true }
-      );
-      const { status, user } = data;
-      setUsername(user);
-      return status
-        ? toast(`Hello ${user}`, {
-            position: "top-right",
-          })
-        : (removeCookie("token"), navigate("/login"));
-    };
-    verifyCookie();
-  }, [cookies, navigate, removeCookie]);
-  const Logout = () => {
-    removeCookie("token");
-    navigate("/signup");
-  };
   
   return (
   <>
